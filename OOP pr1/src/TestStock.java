a1a1741a8f57eee4f99c0006db1531713d589276
acidlirik <p.kur0907@gmail.com>
2024-09-16 22:20:15 +0300

    Add files via upload

---
+++ OOP pr1/src/TestStock.java
@@ -0,0 +1,6 @@
+public class TestStock {
+    public static void main(String[] args) {
+        Stock Stock1 = new Stock("SBER", "ПАО Сбербанк", 281.5, 282.87);
+        System.out.println("Процент изменения стоимости акций равен: " + Stock1.getChangePercent() + "%");
+    }
+}
