f58788cb33ae4a314e3ef1e79c623a73b4f9c8b3
acidlirik <p.kur0907@gmail.com>
2024-09-21 13:10:38 +0300

    Add files via upload

---
+++ OOP pr2/src/Pr2/TestAuthor.java
@@ -0,0 +1,10 @@
+package Pr2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author Gregory = new Author("Kai Angel", "viperrr@gmail.com", 'm');
+        System.out.println(Gregory);
+        Gregory.setEmail("viperrr@gmail.com");
+        System.out.println(Gregory);
+        }
+    }
